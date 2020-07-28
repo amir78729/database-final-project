5b8f89af0c860a838c6e65d1b2a982ae8ba47b39
Amirhossein Alibakhshi <amir78729@gmail.com>
2020-07-29 02:40:30 +0430

    Create tables.sql

---
+++ tables.sql
@@ -0,0 +1,12 @@
+INSERT INTO artist (artistFirstName, artistLastName, artistId, artistPhone, artistAge) VALUES ('artist1', 'artist1', '11111111', '11111111', '10')
+
+INSERT INTO artist (artistFirstName, artistLastName, artistId, artistPhone, artistAge) VALUES ('artist2', 'artist2', '22222222', '22222222', '20')
+
+INSERT INTO artist (artistFirstName, artistLastName, artistId, artistPhone, artistAge) VALUES ('artist3', 'artist3', '33333333', '33333333', '30')
+
+INSERT INTO buyer (buyerFirstName, buyerLastName, buyerPhone) VALUES ('buyer1', 'buyer1', '111')
+
+INSERT INTO buyer (buyerFirstName, buyerLastName, buyerPhone) VALUES ('buyer2', 'buyer2', '222')
+
+INSERT INTO buyer (buyerFirstName, buyerLastName, buyerPhone) VALUES ('buyer3', 'buyer3', '333')
+
